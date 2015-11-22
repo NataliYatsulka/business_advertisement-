436e127fbbc24ff141dd0efb6f9c94a39b4ae985
Natali Yatsulka <yatsulka@gmail.com>
2015-11-22 02:45:38 +0200

    nothing done

--- src/main/java/com/yatsulka/training/App.java
+++ src/main/java/com/yatsulka/training/App.java
@@ -29,6 +29,7 @@ public class App {
 			return "Yes";
 		});
 		ArrayList list = new ArrayList();
+
 		list.add(1);
 		list.add(2);
 		list.add(3);
@@ -37,7 +38,15 @@ public class App {
 		list.add(6);
 		list.add(new Boolean(true));
 		list.add(5);
+		list.add(4, "hi");
+		list.add(5, "it");
+		list.add(8, "is");
+		list.add(10, "a");
+		list.add(12, "good");
+		list.add(13, "day");
+		list.add(14, "!");
 		list.add(6);
+		list.add(343);
 		System.out.println(list);
 		get("/getall", (request, response) -> {
 			return list;
@@ -46,9 +55,18 @@ public class App {
 			return list.get(4);
 		});
 		get("/getbynumberindex/:numb", (request, response) -> {
-			return "list.numb = " + list.get(Integer.valueOf(request.params(":numb"))); //перетворення в інти
+			return "list.numb = " + list.get(Integer.valueOf(request.params(":numb"))); // peretvorennja
+																						// v
+																						// int
 		});
-		
+		post("/postadd", (request, response) -> {
+			return list.add(request.body());
+		});
+
+		delete("/delete_by_index/:numb", (request, response) -> {
+			return "list.numb = " + list.get(Integer.valueOf(request.params(":numb"))) + "   list1 = " + list.get(4)  ;
+			
+		});
 
 	}
 
